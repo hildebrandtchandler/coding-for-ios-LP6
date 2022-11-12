8bdc58a099fed7f568bd174317af69949390be1f
hildebrandtchandler <hildebrandtchandler@gmail.com>
2022-11-12 11:20:48 -0600

    Update sw-reg.js

--- js/sw-reg.js
+++ js/sw-reg.js
@@ -1,7 +1,7 @@
 // does the browser support service workers?
 if ('serviceWorker' in navigator) {
     // then register our service worker
-    navigator.serviceWorker.register('../sw.js')
+    navigator.serviceWorker.register('/sw.js')
         .then(reg => {
             // display a success message
             console.log(`Service Worker Registration (Scope: ${reg.scope})`);
